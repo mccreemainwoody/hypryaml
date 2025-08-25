338b4c6a017d8435736d12d89047239faa2ed6aa
Clément METZ <metz.clement@gmail.com>
2025-08-25 18:54:32 +0200

    feat(workflow): implemented configuration parsing

--- src/workflow/mod.rs
+++ src/workflow/mod.rs
@@ -1,8 +1,23 @@
+use saphyr::{LoadableYamlNode, Yaml};
 use std::path::PathBuf;
+use std::fs;
+
+
+fn parse_configuration(raw_config: &PathBuf) -> Yaml<'_> {
+    let config_string = fs::read_to_string(raw_config)
+        .expect("failed to read file: ");
+
+    let config = Yaml::load_from_str(&config_string).unwrap();
+
+    Yaml::Sequence(config)
+}
 
 
 pub fn apply_configuration(raw_config: &PathBuf) -> Result<(), String> {
-    println!("{}", raw_config.display());
+    let config_wrapped = parse_configuration(&raw_config);
+    let config = &config_wrapped[0];
+
+    let hyprland = &config["hyprland"];
 
     Ok(())
 }
