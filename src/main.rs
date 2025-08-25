0485a468c55747dae2f131a4f9f0a4609ed4e02c
Clément METZ <metz.clement@gmail.com>
2025-08-25 13:25:12 +0200

    feat(main.rs): initialized Clap public API
    added clap to project dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, Subcommand};
+
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct CLI {
+    #[command(subcommand)]
+    command: Commands
+}
+
+
+#[derive(Subcommand)]
+enum Commands {
+    Apply { config: String }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let cli = CLI::parse();
+
+    match &cli.command {
+        Commands::Apply { config } => println!("{}", config),
+    }
 }
