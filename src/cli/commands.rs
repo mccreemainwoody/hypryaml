518ad3edf02a2d106695e52bed4ecfc4b7b960fb
Clément METZ <metz.clement@gmail.com>
2026-05-21 19:21:31 +0200

    feat(cli): apply now displays command result as notification for 5sec

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -1,6 +1,7 @@
 use clap::Subcommand;
 use std::path::PathBuf;
 
+use crate::modules::notify;
 use crate::utils::validate_path;
 use crate::workflow;
 
@@ -25,5 +26,16 @@ pub enum Commands {
 /// A Result object. Ok contains nothing, while Error will contain the reason
 /// of the error as a String.
 pub fn run_apply(config: &PathBuf) -> Result<(), String> {
-    validate_path(config).and_then(|_| workflow::apply_configuration(&config))
+    let result = validate_path(config)
+        .and_then(|_| workflow::apply_configuration(&config));
+
+    let notify_content = match result {
+        Ok(_) => Ok("configuration succesfully applied"),
+        Err(ref reason) => Err(reason.as_str()),
+    };
+    let notify_timeout: u32 = 5000;
+
+    notify::send_notification(&notify_content, &notify_timeout);
+
+    result
 }
