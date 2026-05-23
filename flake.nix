34e291696955d5297167963abdb93ed0144c6386
Clément METZ <metz.clement@gmail.com>
2026-05-23 14:13:14 +0200

    style(nix): defined formatter to alejandra

--- flake.nix
+++ flake.nix
@@ -1,44 +1,49 @@
 {
-    description = "hypryaml flake definition";
+  description = "hypryaml flake definition";
 
-    inputs = {
-        systems.url = "systems";
+  inputs = {
+    systems.url = "systems";
 
-        nixpkgs.url = "nixpkgs/nixos-unstable";
-        flake-utils = {
-            url = "github:numtide/flake-utils";
-            inputs.systems.follows = "systems";
-        };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
     };
+  };
 
-    outputs = { self, nixpkgs, flake-utils, ... } @ _:
-        flake-utils.lib.eachDefaultSystem (system: 
-            let
-                pkgs = nixpkgs.legacyPackages.${system};
-            in
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ _:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        formatter = pkgs.alejandra;
+        packages = rec {
+          hypryaml =
+            pkgs.rustPlatform.buildRustPackage
             {
-                packages = rec {
-                    hypryaml = pkgs.rustPlatform.buildRustPackage
-                    {
-                        pname = "hypryaml";
-                        version = "0.2.1";
+              pname = "hypryaml";
+              version = "0.2.1";
 
-                        src = ./.; 
-                        cargoLock = { lockFile = ./Cargo.lock; };
+              src = ./.;
+              cargoLock = {lockFile = ./Cargo.lock;};
 
-                        meta = {
-                            description = "Dynamically set Hypr values using YAML !";
-                            license = "MIT";
-                            homepage = "https://github.com/mccreemainwoody/hypryaml";
-                            systems = flake-utils.defaultSystems;
-                        };
-                    };
-                    default = hypryaml;
-                };
-                devShell = pkgs.mkShell {
-                    packages = with pkgs;
-                        [ rustc cargo rustfmt rust-analyzer libnotify ];
-                };
-            }
-        );
+              meta = {
+                description = "Dynamically set Hypr values using YAML !";
+                license = "MIT";
+                homepage = "https://github.com/mccreemainwoody/hypryaml";
+                systems = flake-utils.defaultSystems;
+              };
+            };
+          default = hypryaml;
+        };
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [rustc cargo rustfmt rust-analyzer libnotify];
+        };
+      }
+    );
 }
